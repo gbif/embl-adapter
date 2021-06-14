13ea0a7fdb2ea82afe91a4c154fb95496c0be43c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2021-06-14 10:11:10 +0200

    Deduplication update

--- src/main/resources/sql-dataset-common.sql
+++ src/main/resources/sql-dataset-common.sql
@@ -18,6 +18,24 @@ SELECT ed.accession,
        et."order",
        et.family,
        et.genus
-FROM embl_data ed
+FROM (
+         SELECT row_number()
+                over (PARTITION BY tax_id, scientific_name, collection_date, location, country, collected_by, identified_by, sample_accession ORDER BY tax_id) as row_num,
+                accession,
+                sample_accession,
+                location,
+                country,
+                identified_by,
+                collected_by,
+                collection_date,
+                specimen_voucher,
+                sequence_md5,
+                scientific_name,
+                tax_id,
+                altitude,
+                sex,
+                description
+         FROM embl_data1
+         WHERE scientific_name != 'Homo sapiens') as ed
          LEFT JOIN ena_taxonomy et ON ed.tax_id = et.taxon_id
-WHERE ed.scientific_name != 'Homo sapiens'
+WHERE ed.row_num < 50
